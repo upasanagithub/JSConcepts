13b7a050222729926a4c9f1a7cbc9c8b0c23940b
upasana.mahanta <ups.win@gmail.com>
2019-10-25 22:49:45 +0530

    Commit

---
+++ prototypal_inheritance_ES5.js
@@ -0,0 +1,35 @@
+/*
+1. Protoypal Inheritance
+2. Use of static function in ES5
+*/
+
+
+function Book(title,author){
+  this.title = title;
+  this.author = author;
+}
+
+const book1 = new Book('Harry Potter','J.K. Rowling');
+const book2 = new Book('Veronica Must Die','Paulo Coelho');
+
+Book.prototype.getMsg = function(){
+  return "The author of the book "+this.title+" is "+this.author;
+}
+
+Book.staticMsg = function(){
+  return "Here is my static msg";
+}
+
+function Magazine(title, author ,category){
+  this.title = title;
+  this.author = author;
+  this.category = category;
+}
+Magazine.prototype = Book.prototype;
+
+const mag1 = new Magazine('Vogue','Anaita Shroff', 'Fashion');
+
+console.log(book1.getMsg());
+console.log(book2.getMsg());
+console.log(Book.staticMsg());
+console.log(mag1.getMsg());
