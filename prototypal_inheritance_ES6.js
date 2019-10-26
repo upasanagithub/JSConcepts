6a8031fdfca11d93e3883f954ad49b4563f35a58
upasana.mahanta <ups.win@gmail.com>
2019-10-26 10:20:00 +0530

    Commit

--- prototypal_inheritance_ES6.js
+++ prototypal_inheritance_ES6.js
@@ -7,7 +7,22 @@ class Book{
     return "The author of the book "+this.title+" is "+this.author;
   }
 }
+
+class Magazine extends Book{
+  constructor(category, title, author){
+    super(title, author);
+    this.category = category;
+  }
+}
+
+
+
 const book1 = new Book('Harry Potter', 'J.K. Rowling');
 const book2 = new Book('Veronica Wants to Die', 'Paulo Coelho');
 
+const mag1 = new Magazine('Fashion', 'Vogue', 'Anaita Shroff');
+
+
+
 console.log(book1.getMsg());
+console.log(mag1.getMsg());
